ce6f4582683863dc2b979c304a5948ab49e263c0
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2024-11-04 00:20:16 -0800

    Add WebSocket support and update dependencies for actix-web

--- src/workingmain.rs
+++ src/workingmain.rs
@@ -1,9 +1,6 @@
-use actix::prelude::*;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use actix_web_actors::ws;
 use rusqlite::{params, Connection, Result};
 use serde::Serialize;
-use std::time::{Duration, Instant};
 
 #[derive(Serialize)]
 struct SensorData {
@@ -61,34 +58,6 @@ async fn humi() -> impl Responder {
     }
 }
 
-struct MyWebSocket;
-
-impl Actor for MyWebSocket {
-    type Context = ws::WebsocketContext<Self>;
-
-    fn started(&mut self, ctx: &mut Self::Context) {
-        ctx.run_interval(Duration::from_secs(5), |act, ctx| {
-            // Here you would send a message to the client.
-            // For example, you could query the database and send updated sensor data.
-        });
-    }
-}
-
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWebSocket {
-    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
-        match msg {
-            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
-            Ok(ws::Message::Text(text)) => ctx.text(text),
-            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
-            _ => (),
-        }
-    }
-}
-
-async fn ws_index(r: web::Payload, stream: web::Query<String>) -> impl Responder {
-    ws::start(MyWebSocket {}, &stream, r)
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
@@ -96,7 +65,6 @@ async fn main() -> std::io::Result<()> {
             .route("/tempc", web::get().to(tempc))
             .route("/tempf", web::get().to(tempf))
             .route("/humi", web::get().to(humi))
-            .route("/ws/", web::get().to(ws_index))
     })
     .bind("10.0.4.60:8080")?
     .run()
