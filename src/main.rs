32ad9deebdb41e30bb41a046715170376a836904
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2024-11-13 05:56:39 -0800

    Add endpoints to retrieve yesterday's temperature and humidity data

--- src/main.rs
+++ src/main.rs
@@ -37,8 +37,38 @@ fn get_current_date() -> String {
     local.format("%Y-%m-%d").to_string()
 }
 
+fn get_yesterdays_date() -> String {
+    let local: DateTime<Local> = Local::now();
+    let yesterdays_date = local - chrono::Duration::days(1);
+    yesterdays_date.format("%Y-%m-%d").to_string()
+}
+
 async fn todays_data() -> Result<Vec<SensorData>> {
     let date = get_current_date();
+    let conn = Connection::open("/usr/share/dht11rs/dht11rs/sensor_data.db")?;
+    let stmstr = format!(
+        "SELECT id, tempc, tempf, humi, date, time, timestamp FROM sensorhour WHERE date = '{}'",
+        date
+    );
+    let mut stmt = conn.prepare(&stmstr)?;
+    let sensor_data_iter = stmt.query_map(params![], |row| {
+        Ok(SensorData {
+            id: row.get(0)?,
+            tempc: row.get(1)?,
+            tempf: row.get(2)?,
+            humi: row.get(3)?,
+            date: row.get(4)?,
+            time: row.get(5)?,
+            timestamp: row.get(6)?,
+        })
+    })?;
+    
+    let sensor_data_vec: Vec<SensorData> = sensor_data_iter.filter_map(Result::ok).collect();
+    Ok(sensor_data_vec)
+}
+
+async fn yesterdays_data() -> Result<Vec<SensorData>> {
+    let date = get_yesterdays_date();
     let conn = Connection::open("/usr/share/dht11rs/dht11rs/sensor_data.db")?;
     let stmstr = format!(
         "SELECT id, tempc, tempf, humi, date, time, timestamp FROM sensorhour WHERE date = '{}'",
@@ -73,7 +103,19 @@ async fn get_todays_tempf() -> impl Responder {
         }
     }
 }
-// }
+
+async fn get_yesterdays_tempf() -> impl Responder {
+    match yesterdays_data().await {
+        Ok(sensor_data) => {
+            let tempf_vec: Vec<String> = sensor_data.iter().map(|x| x.tempf.clone()).collect();
+            HttpResponse::Ok().json(tempf_vec)
+        }
+        Err(e) => {
+            eprintln!("Error querying the database: {}", e);
+            HttpResponse::InternalServerError().body("Internal Server Error")
+        }
+    }
+}
 
 async fn get_todays_tempc() -> impl Responder {
     match todays_data().await {
@@ -88,8 +130,34 @@ async fn get_todays_tempc() -> impl Responder {
     }
 }
 
+async fn get_yesterdays_tempc() -> impl Responder {
+    match yesterdays_data().await {
+        Ok(sensor_data) => {
+            let tempc_vec: Vec<String> = sensor_data.iter().map(|x| x.tempc.clone()).collect();
+            HttpResponse::Ok().json(tempc_vec)
+        }
+        Err(e) => {
+            eprintln!("Error querying the database: {}", e);
+            HttpResponse::InternalServerError().body("Internal Server Error")
+        }
+    }
+}
+
 async fn get_todays_humi() -> impl Responder {
     match todays_data().await {
+        Ok(sensor_data) => {
+            let humi_vec: Vec<String> = sensor_data.iter().map(|x| x.humi.clone()).collect();
+            HttpResponse::Ok().json(humi_vec)
+        }
+        Err(e) => {
+            eprintln!("Error querying the database: {}", e);
+            HttpResponse::InternalServerError().body("Internal Server Error")
+        }
+    }
+}
+
+async fn get_yesterdays_humi() -> impl Responder {
+    match yesterdays_data().await {
         Ok(sensor_data) => {
             let humi_vec: Vec<String> = sensor_data.iter().map(|x| x.humi.clone()).collect();
             HttpResponse::Ok().json(humi_vec)
@@ -157,6 +225,10 @@ async fn main() -> std::io::Result<()> {
             .route("/todays_tempf", web::get().to(get_todays_tempf))
             .route("/todays_humi", web::get().to(get_todays_humi))
             .route("/todays_tempc", web::get().to(get_todays_tempc))
+            .route("/yesterdays_tempf", web::get().to(get_yesterdays_tempf))
+            .route("/yesterdays_humi", web::get().to(get_yesterdays_humi))
+            .route("/yesterdays_tempc", web::get().to(get_yesterdays_tempc))
+
         })
     .bind("10.0.4.60:8080")?
     .run()
